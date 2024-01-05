f77b823f4aa10389fbd16fd0e555fb5e4f985eb6
iShell <aysel.abdullayeva.gitlab@gmail.com>
2024-01-05 21:14:09 +0400

    create about component

--- app/we-are-nac/page.tsx
+++ app/we-are-nac/page.tsx
@@ -1,12 +1,23 @@
-import NOA from '@/components/WeAreNac/NOA'
-import React from 'react'
+"use client";
+import NOA from "@/components/WeAreNac/NOA";
+import About from "@/components/shared/About";
+import FetchData from "@/helpers/FetchData";
 
 const page = () => {
+  const { cachedData } = FetchData(["who"]);
+  const section_2 = cachedData?.who?.section_2;
+  const section_3_4 = cachedData?.who?.section_3_4;
   return (
     <>
-        <NOA />
+      <NOA />
+      <About
+        title={section_2?.section_two_title_1_en}
+        text={section_2?.section_two_text_en}
+        url={section_2?.section_two_src}
+        bg={section_2?.section_two_bg}
+      />
     </>
-  )
-}
+  );
+};
 
-export default page
+export default page;
